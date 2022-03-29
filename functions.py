b752b3aaf98c2ca3a3717cfc103a64238b673f72
ase1541 <sierralvaro56@gmail.com>
2022-03-29 13:15:02 +0200

    Update2: Before project milestone

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 #LIBRARIES
 import pandas as pd
+import numpy as np
 from statsmodels.tsa.stattools import adfuller
 
 #Dickey Fuller Test
@@ -15,4 +16,24 @@ def dickey_fuller(timeseries):
     if adft[0] < adft[4]["5%"]:
         print ("Reject Ho - Time Series is Stationary")
     else:
-        print ("Failed to Reject Ho - Time Series is Non-Stationary")
+        print ('Failed to Reject Ho - Time Series is Non-Stationary')
+
+
+#Accuracy function information
+# Accuracy metrics
+def forecast_accuracy(forecast, actual):
+    mape = np.mean(np.abs(forecast - actual)/np.abs(actual))  # MAPE
+    me = np.mean(forecast - actual)             # ME
+    mae = np.mean(np.abs(forecast - actual))    # MAE
+    mpe = np.mean((forecast - actual)/actual)   # MPE
+    rmse = np.mean((forecast - actual)**2)**.5  # RMSE
+    corr = np.corrcoef(forecast, actual)[0,1]   # corr
+    mins = np.amin(np.hstack([forecast[:,None],
+                              actual[:,None]]), axis=1)
+    maxs = np.amax(np.hstack([forecast[:,None],
+                              actual[:,None]]), axis=1)
+    minmax = 1 - np.mean(mins/maxs)             # minmax
+
+    return({'mape':mape, 'me':me, 'mae': mae,
+            'mpe': mpe, 'rmse':rmse,
+            'corr':corr, 'minmax':minmax})
